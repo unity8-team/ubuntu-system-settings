8bfc59a7717ec7ca62d5751b16974018758e79a7
Iain Lane <iain.lane@canonical.com>
2013-07-24 13:43:20 +0000

    Don't register the TimeZoneLocationModel type as it's not used in QML directly any more

--- plugins/time-date/plugin.cpp
+++ plugins/time-date/plugin.cpp
@@ -29,7 +29,6 @@ void BackendPlugin::registerTypes(const char *uri)
     Q_ASSERT(uri == QLatin1String("Ubuntu.SystemSettings.TimeDate"));
     
     qmlRegisterType<TimeDate>(uri, 1, 0, "UbuntuTimeDatePanel");
-    qmlRegisterType<TimeZoneLocationModel>();
 }
 
 void BackendPlugin::initializeEngine(QQmlEngine *engine, const char *uri)
