853227b38617b9e5b5dbb6131006c3f1e4482a1c
Sebastien Bacher <seb128@ubuntu.com>
2013-08-01 18:38:12 +0200

    sound: split out helper function in a new source so we can reuse it

---
+++ plugins/sound/utilities.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2013 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ * Sebastien Bacher <sebastien.bacher@canonical.com>
+ *
+ */
+
+function buildDisplayName(sound) {
+    return sound.split('/').pop().split('.').slice(0,-1).join(" ")
+}
+
+function buildSoundValues(sounds) {
+    return sounds.map(buildDisplayName)
+}
