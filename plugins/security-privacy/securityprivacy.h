b7b830706c0470bf0dab60d2a0efa0d1e5578820
Iain Lane <iain.lane@canonical.com>
2013-08-29 14:20:50 +0000

    Fix authors and copyright years

--- plugins/security-privacy/securityprivacy.h
+++ plugins/security-privacy/securityprivacy.h
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authors: Michael Terry <michael.terry@canonical.com>
+ *          Iain Lane <iain.lane@canonical.com>
  */
 
 #ifndef SECURITYPRIVACY_H
