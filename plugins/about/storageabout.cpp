99eea7cd3753cec17e95a846ad07e8df1d6cdbd9
Iain Lane <iain.lane@canonical.com>
2013-09-25 16:01:34 +0000

    Free the GError

--- plugins/about/storageabout.cpp
+++ plugins/about/storageabout.cpp
@@ -112,10 +112,12 @@ static void measure_finished(GObject *source_object,
         if (g_error_matches (err, G_IO_ERROR, G_IO_ERROR_CANCELLED)) {
             delete data->finished;
             g_object_unref (file);
+            g_error_free (err);
             return;
         } else {
             qWarning() << "Measuring of" << g_file_get_path (file)
                        << "failed:" << err->message;
+            g_error_free(err);
         }
     }
 
