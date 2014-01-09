4128449ca0a4ba5d8a179fff9c0eb746092e47cc
Iain Lane <iain.lane@canonical.com>
2014-01-09 13:02:44 +0000

    Add comment & rename script

--- debian/ubuntu-system-settings-sound-gsettings-to-accountsservice.py
+++ debian/ubuntu-system-settings-sound-gsettings-to-accountsservice.py
@@ -18,6 +18,9 @@
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+# Migrate ubuntu-system-settings sound panel settings from GSettings to
+# AccountsService
+ 
 import dbus
 import os
 
