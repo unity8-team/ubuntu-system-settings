5b806ef4b1972ea9e25d33c614d0b72fe30cc207
Diego Sarmentero <diego.sarmentero@gmail.com>
2014-05-12 11:36:18 -0300

    integrating click updates checks

--- plugins/system-update/plugin/update-plugin.cpp
+++ plugins/system-update/plugin/update-plugin.cpp
@@ -24,9 +24,20 @@
 #include <QStringList>
 #include <SystemSettings/ItemBase>
 
+#include <token.h>
+#include <ssoservice.h>
+#include <QProcess>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonValue>
+#include <QHash>
 #include "../system_update.h"
+#include "../update.h"
+#include "../network/network.h"
 
 using namespace SystemSettings;
+using namespace UbuntuOne;
 using namespace UpdatePlugin;
 
 class UpdateItem: public ItemBase
@@ -38,11 +49,34 @@ public:
     void setVisibility(bool visible);
     ~UpdateItem();
 
+Q_SIGNALS:
+    void checkFinished();
+    void modelChanged();
+    void updatesNotFound();
+    void credentialsNotFound();
+    void updateAvailableFound(bool downloading);
+    void errorFound();
+    void downloadModeChanged();
+    void systemUpdateDownloaded();
+    void updateProcessFailed(QString message);
+    void systemUpdateFailed(int consecutiveFailureCount, QString lastReason);
+
 private Q_SLOTS:
     void changeVisibility(const QString&, Update*);
+    void systemUpdatePaused(int value);
+    void processOutput();
+    void processUpdates();
+    void downloadUrlObtained(const QString &packagename, const QString &url);
+    void handleCredentialsFound(Token token);
+    void clickTokenReceived(Update *app, const QString &clickToken);
 
 private:
     SystemUpdate m_systemUpdate;
+    Token m_token;
+    UpdatePlugin::Network m_network;
+    QProcess m_process;
+    SSOService m_service;
+    QHash<QString, Update*> m_apps;
 };
 
 UpdateItem::UpdateItem(const QVariantMap &staticData, QObject *parent):
@@ -50,10 +84,65 @@ UpdateItem::UpdateItem(const QVariantMap &staticData, QObject *parent):
     m_systemUpdate(this)
 {
     setVisibility(false);
+    // SYSTEM UPDATE
     QObject::connect(&m_systemUpdate, SIGNAL(updateAvailable(const QString&, Update*)),
                   this, SLOT(changeVisibility(const QString&, Update*)));
 
+    // SSO SERVICE
+    QObject::connect(&m_service, SIGNAL(credentialsFound(const Token&)),
+                     this, SLOT(handleCredentialsFound(Token)));
+    // PROCESS
+    QObject::connect(&m_process, SIGNAL(finished(int)),
+                  this, SLOT(processOutput()));
+    // NETWORK
+    QObject::connect(&m_network, SIGNAL(updatesFound()),
+                  this, SLOT(processUpdates()));
+
     m_systemUpdate.checkForUpdate();
+    m_service.getCredentials();
+}
+
+void UpdateItem::handleCredentialsFound(Token token)
+{
+    m_token = token;
+    QStringList args("list");
+    args << "--manifest";
+    QString command("click");
+    m_process.start(command, args);
+}
+
+void UpdateItem::processOutput()
+{
+    QString output(m_process.readAllStandardOutput());
+
+    QJsonDocument document = QJsonDocument::fromJson(output.toUtf8());
+
+    QJsonArray array = document.array();
+
+    int i;
+    for (i = 0; i < array.size(); i++) {
+        QJsonObject object = array.at(i).toObject();
+        QString name = object.value("name").toString();
+        QString title = object.value("title").toString();
+        QString version = object.value("version").toString();
+        Update *app = new Update();
+        app->initializeApplication(name, title, version);
+        m_apps[app->getPackageName()] = app;
+    }
+
+    m_network.checkForNewVersions(m_apps);
+}
+
+
+void UpdateItem::processUpdates()
+{
+    foreach (QString id, m_apps.keys()) {
+        Update *app = m_apps.value(id);
+        if(app->updateRequired()) {
+            setVisibility(true);
+            break;
+        }
+    }
 }
 
 void UpdateItem::changeVisibility(const QString&, Update*)
