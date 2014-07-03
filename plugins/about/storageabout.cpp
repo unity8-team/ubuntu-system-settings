60ad8afd00732d9c39ae672fd0e95819850a5ce1
Ubuntu daily release <ps-jenkins@lists.canonical.com>
2014-07-03 15:29:07 +0000

    Import upstream version 0.3+14.10.20140703.1

--- plugins/about/storageabout.cpp
+++ plugins/about/storageabout.cpp
@@ -249,6 +249,7 @@ void StorageAbout::setSortRole(ClickModel::Roles newRole)
     m_clickFilterProxy.sort(0, newRole == ClickModel::InstalledSizeRole ?
                                 Qt::DescendingOrder :
                                 Qt::AscendingOrder);
+    m_clickFilterProxy.invalidate();
     Q_EMIT(sortRoleChanged());
 }
 
