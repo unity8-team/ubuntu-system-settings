25e3a07f39c67b0738e8ab11c2ddada6845b0d86
Leo Arias <leo.arias@canonical.com>
2014-07-11 08:17:51 -0600

    Reverted the license changes for now.

--- tests/autopilot/ubuntu_system_settings/tests/test_search.py
+++ tests/autopilot/ubuntu_system_settings/tests/test_search.py
@@ -1,20 +1,9 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+# Copyright 2013 Canonical
 #
-# Copyright 2013, 2014 Canonical
-#
-# This file is part of ubuntu-system-settings
-#
-# ubuntu-system-settings is free software: you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 3.
-#
-# ubuntu-system-settings is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
 
 from testtools.matchers import Equals
 from autopilot.matchers import Eventually
