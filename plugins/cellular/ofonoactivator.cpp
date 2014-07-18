f5941e2f0fdb6a9dd70f90177ec1eee465597d8d
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2014-07-18 16:49:15 +0300

    Fill in ofono activator code.

--- plugins/cellular/ofonoactivator.cpp
+++ plugins/cellular/ofonoactivator.cpp
@@ -19,9 +19,97 @@
 
 #include "ofonoactivator.h"
 
+#include"nm_manager_proxy.h"
+#include"nm_settings_proxy.h"
+#include"nm_settings_connection_proxy.h"
+#include<QCoreApplication>
+
+QString nmService("org.freedesktop.NetworkManager");
+QString nmSettingsPath("/org/freedesktop/NetworkManager/Settings");
+QString nmPath("/org/freedesktop/NetworkManager");
+
+typedef QMap<QString,QVariantMap> ConfigurationData;
+Q_DECLARE_METATYPE(ConfigurationData)
+
+QDBusObjectPath detectConnection(const QString &ofonoContext) {
+    auto ofonoContextBase = ofonoContext.split('/').back();
+
+    OrgFreedesktopNetworkManagerSettingsInterface settings(nmService, nmSettingsPath,
+            QDBusConnection::systemBus());
+    auto reply = settings.ListConnections();
+    reply.waitForFinished();
+    if(!reply.isValid()) {
+        qWarning() << "Error getting connection list: " << reply.error().message() << "\n";
+    }
+    auto connections = reply.value(); // Empty list if failed.
+
+    for(const auto &c : connections) {
+        OrgFreedesktopNetworkManagerSettingsConnectionInterface connProxy(nmService,
+                c.path(), QDBusConnection::systemBus());
+        auto reply2 = connProxy.GetSettings();
+        reply2.waitForFinished();
+        if(!reply2.isValid()) {
+            qWarning() << "Error getting property: " << reply2.error().message() << "\n";
+            continue;
+        }
+        auto settings = reply2.value();
+        auto context = settings["connection"]["id"].toString();
+        auto contextBase = context.split('/').back();
+        if(contextBase == ofonoContextBase) {
+            return c;
+        }
+    }
+    return QDBusObjectPath("");
+}
+
+QDBusObjectPath detectDevice(const QString &ofonoContext) {
+    auto ofonoContextBase = ofonoContext.split('/')[1];
+    OrgFreedesktopNetworkManagerInterface nm(nmService, nmPath, QDBusConnection::systemBus());
+    auto reply = nm.GetDevices();
+    reply.waitForFinished();
+    auto devices = reply.value();
+
+    for(const auto &device : devices) {
+        QDBusInterface iface(nmService, device.path(), "org.freedesktop.DBus.Properties",
+                QDBusConnection::systemBus());
+        QDBusReply<QDBusVariant> ifaceReply = iface.call("Get",
+                "org.freedesktop.NetworkManager.Device", "Interface");
+        if(!ifaceReply.isValid()) {
+            qWarning() << "Error getting property: " << ifaceReply.error().message() << "\n";
+            continue;
+        }
+        auto devIface = ifaceReply.value().variant().toString();
+        if(devIface == ofonoContextBase) {
+            return device;
+        }
+    }
+    return QDBusObjectPath("");
+}
+
+void activateOfono(QDBusObjectPath connection, QDBusObjectPath device) {
+    OrgFreedesktopNetworkManagerInterface nm(nmService, nmPath, QDBusConnection::systemBus());
+    nm.ActivateConnection(connection, device, QDBusObjectPath("/"));
+}
+
 OfonoActivator::OfonoActivator(QObject *parent) : QObject(parent) {
+    static bool isRegistered = false;
+    if(!isRegistered) {
+        qDBusRegisterMetaType<ConfigurationData>();
+        isRegistered = true;
+    }
 }
 
 Q_INVOKABLE bool OfonoActivator::activate(const QString ofonoContext) {
+    auto conn = detectConnection(ofonoContext);
+    if(conn.path() == "") {
+        qWarning() << "Could not detect connection object to use for Ofono activation.\n";
+        return false;
+    }
+    auto dev = detectDevice(ofonoContext);
+    if(dev.path() == "") {
+        qWarning() << "Could not detect device object to use for Ofono activation.\n";
+        return false;
+    }
+    activateOfono(conn, dev);
     return true;
 }
