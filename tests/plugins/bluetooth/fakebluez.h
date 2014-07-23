d7d03c0fe8344d1db0b8754e4a0327778037abdb
Mathieu Trudel-Lapierre <mathieu-tl@ubuntu.com>
2014-07-22 22:45:10 -0400

    Initialize the bluetooth test code based on system-update tests.

---
+++ tests/plugins/bluetooth/fakebluez.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 Canonical Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of version 3 of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef FAKEBLUEZ_H
+#define FAKEBLUEZ_H
+
+#include <QObject>
+#include <QHash>
+#include <QString>
+
+namespace FakeBluez {
+
+class FakeBluez : public QObject
+{
+    Q_OBJECT
+
+private:
+    QDBusObjectPath m_currentAdapter;
+
+public:
+    explicit FakeBluez(QObject *parent = 0);
+
+    QString addAdapter(const QString &name, const QString &address);
+    QString addDevice(const QString &name, const QString &address);
+    
+};
+
+}
+
+#endif // FAKEBLUEZ_H
