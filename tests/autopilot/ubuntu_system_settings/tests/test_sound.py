0e5d45de013889d84fbeff0e9314bf5d3e151e1d
Omer Akram <om26er@ubuntu.com>
2014-07-25 04:48:28 +0500

    copy over tests from other branch, still not working

--- tests/autopilot/ubuntu_system_settings/tests/test_sound.py
+++ tests/autopilot/ubuntu_system_settings/tests/test_sound.py
@@ -5,7 +5,10 @@
 # under the terms of the GNU General Public License version 3, as published
 # by the Free Software Foundation.
 
+from autopilot.matchers import Eventually
 from testtools.matchers import Equals, NotEquals
+
+import autopilot.platform
 
 from ubuntu_system_settings.tests import SoundBaseTestCase
 from ubuntu_system_settings.utils.i18n import ugettext as _
@@ -33,3 +36,33 @@ class SoundTestCase(SoundBaseTestCase):
         self.system_settings.main_view.pointing_device.click_object(kbd_snd)
         self.assertThat(
             kbd_snd.get_properties()["checked"], NotEquals(current_value))
+
+    @skipIf(platform.model() is 'Desktop', 'Phones only')
+    def test_ringtone_setting_change_in_ui(self):
+        """Ensure ringtone change is shown in UI."""
+        ringtone = 'Supreme'
+        sounds_list = self.sound_page.open_ringtone_selector()
+        sounds_list.choose_ringtone(ringtone)
+
+        sounds_list.go_back_to_sound_page()
+
+        self.assertThat(
+            self.sound_page.get_ringtone_setting_button_current_value(),
+            Eventually(Equals(ringtone))
+        )
+
+    @skipIf(platform.model() is 'Desktop', 'Phones only')
+    def test_ringtone_setting_change_in_backend(self):
+        """Ensure ringtone change saves in backend."""
+        ringtone = 'Supreme'
+        sounds_list = self.sound_page.open_ringtone_selector()
+        current_ringtone = sounds_list.choose_ringtone(ringtone)
+
+        self.assertThat(
+            current_ringtone.selected, Eventually(Equals(True))
+        )
+        self.assertThat(
+            lambda: helpers.get_current_ringtone_from_backend().endswith(
+                ringtone + '.ogg'), Eventually(Equals(True))
+        )
+
