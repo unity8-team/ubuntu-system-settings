d0b7c1f796fabc340d07923613e5362d7a3f5f5b
Ken VanDine <ken.vandine@canonical.com>
2014-08-21 00:40:27 -0400

    handle reseting activity-timeout and idle-delay in gsettings in cleanup

--- tests/autopilot/ubuntu_system_settings/tests/test_security.py
+++ tests/autopilot/ubuntu_system_settings/tests/test_security.py
@@ -6,6 +6,7 @@
 # by the Free Software Foundation.
 
 from gi.repository import Gio
+from time import sleep
 from testtools.matchers import Equals, NotEquals
 
 from ubuntu_system_settings.tests import SecurityBaseTestCase
@@ -17,15 +18,40 @@ from ubuntuuitoolkit import emulators as toolkit_emulators
 class SecurityTestCase(SecurityBaseTestCase):
     """ Tests for Security Page """
 
-    def _get_activity_timeout(self):
+    def setUp(self):
+        super(SecurityTestCase, self).setUp()
         prps = self.system_settings.main_view.security_page.get_properties()
-        usePowerd = prps['usePowerd']
-        if usePowerd:
+        self.use_powerd = prps['usePowerd']
+        if self.use_powerd:
             gsettings = Gio.Settings.new('com.ubuntu.touch.system')
-            return gsettings.get_uint('activity-timeout')
+            prev = gsettings.get_uint('activity-timeout')
+            self.addCleanup(
+                self.set_prev_activity_timeout, gsettings, prev)
         else:
             gsettings = Gio.Settings.new('org.gnome.desktop.session')
-            return gsettings.get_uint('idle-delay')
+            prev = gsettings.get_uint('idle-delay')
+            self.addCleanup(
+                self.set_prev_idle_delay, gsettings, prev)
+
+    def set_prev_idle_delay(self, gsettings, prev):
+        gsettings.set_uint('idle-delay', prev)
+        # wait for gsettings
+        sleep(0.2)
+
+    def set_prev_activity_timeout(self, gsettings, prev):
+        gsettings.set_uint('activity-timeout', prev)
+        # wait for gsettings
+        sleep(0.2)
+
+    def _get_activity_timeout(self):
+        if self.use_powerd:
+            gsettings = Gio.Settings.new('com.ubuntu.touch.system')
+            prev = gsettings.get_uint('activity-timeout')
+            return prev
+        else:
+            gsettings = Gio.Settings.new('org.gnome.desktop.session')
+            prev = gsettings.get_uint('idle-delay')
+            return prev
 
     def _go_to_phone_lock(self):
         selector = self.security_page.select_single(
@@ -39,6 +65,22 @@ class SecurityTestCase(SecurityBaseTestCase):
             objectName="lockTimeout"
         )
         self.system_settings.main_view.scroll_to_and_click(selector)
+
+    def _get_sleep_selector(self):
+        self._go_to_sleep_values()
+        sleep_values_page = self.system_settings.main_view.select_single(
+            objectName='sleepValues')
+        self.assertThat(
+            sleep_values_page,
+            NotEquals(None)
+        )
+        self._go_to_sleep_values()
+        sleep_values_page = self.system_settings.main_view.select_single(
+            objectName='sleepValues')
+        return sleep_values_page.select_single(
+            toolkit_emulators.ItemSelector,
+            objectName="sleepSelector"
+        )
 
     def test_security_page(self):
         """ Checks whether Security page is available """
@@ -99,27 +141,12 @@ class SecurityTestCase(SecurityBaseTestCase):
                 ('{:d} minutes').format(int(actTimeout/60))
             )
 
-    def _get_sleep_selector(self):
-        self._go_to_sleep_values()
-        sleep_values_page = self.system_settings.main_view.select_single(
-            objectName='sleepValues')
-        self.assertThat(
-            sleep_values_page,
-            NotEquals(None)
-        )
-        self._go_to_sleep_values()
-        sleep_values_page = self.system_settings.main_view.select_single(
-            objectName='sleepValues')
-        return sleep_values_page.select_single(
-            toolkit_emulators.ItemSelector,
-            objectName="sleepSelector"
-        )
-
     def test_idle_never_timeout(self):
         selector = self._get_sleep_selector()
         to_select = selector.select_single(
             'OptionSelectorDelegate', text='Never')
         self.system_settings.main_view.pointing_device.click_object(to_select)
+        to_select.selected.wait_for(True)
         actTimeout = self._get_activity_timeout()
         self.assertEquals(actTimeout, 0)
         selected_delegate = selector.select_single(
@@ -131,6 +158,7 @@ class SecurityTestCase(SecurityBaseTestCase):
         to_select = selector.select_single(
             'OptionSelectorDelegate', text='After 4 minutes')
         self.system_settings.main_view.pointing_device.click_object(to_select)
+        to_select.selected.wait_for(True)
         actTimeout = self._get_activity_timeout()
         self.assertEquals(actTimeout, 240)
         selected_delegate = selector.select_single(
