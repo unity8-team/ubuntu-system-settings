e13c2d51d86e3747ca538a6e1143e04f6555bdfd
Ubuntu daily release <ps-jenkins@lists.canonical.com>
2014-09-04 04:39:47 +0000

    Import upstream version 0.3+14.10.20140904.1

--- plugins/bluetooth/bluetooth.cpp
+++ plugins/bluetooth/bluetooth.cpp
@@ -40,7 +40,7 @@ Bluetooth::Bluetooth(const QDBusConnection &dbus, QObject *parent):
     // export our Agent to handle pairing requests
     new AgentAdaptor(&m_agent);
     if(!m_dbus.registerObject(DBUS_AGENT_PATH, &m_agent))
-        qFatal("Couldn't register agent at " DBUS_AGENT_PATH);
+        qCritical() << "Couldn't register agent at" << DBUS_AGENT_PATH;
 
     m_connectedDevices.filterOnConnections(Device::Connection::Connected |
                                            Device::Connection::Connecting |
