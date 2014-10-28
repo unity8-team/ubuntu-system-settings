6424a5fd5d7c496f9d4c4cb616fbe6a38536fdc0
Ubuntu daily release <ps-jenkins@lists.canonical.com>
2014-10-28 20:44:34 +0000

    Import upstream version 0.3+15.04.20141028.1

--- plugins/system-update/update_manager.h
+++ plugins/system-update/update_manager.h
@@ -80,8 +80,7 @@ Q_SIGNALS:
     void rebooting(bool status);
     
 public:
-    explicit UpdateManager(QObject *parent = 0);
-    ~UpdateManager();
+    static UpdateManager *instance();
 
     Q_INVOKABLE void checkUpdates();
     Q_INVOKABLE void startDownload(const QString &packagename);
@@ -97,6 +96,8 @@ public:
     QDateTime lastUpdateDate() { return m_systemUpdate.lastUpdateDate(); }
     QString currentUbuntuBuildNumber() { return m_systemUpdate.currentUbuntuBuildNumber(); }
     QString currentDeviceBuildNumber() { return m_systemUpdate.currentDeviceBuildNumber(); }
+    bool checkTarget() { return m_systemUpdate.checkTarget(); }
+
 
 #ifdef TESTS
     // For testing purposes
@@ -115,6 +116,10 @@ public Q_SLOTS:
     void registerSystemUpdate(const QString& packageName, Update *update);
     void systemUpdateNotAvailable();
 
+protected:
+    explicit UpdateManager(QObject *parent = 0);
+    ~UpdateManager();
+
 private Q_SLOTS:
     void clickUpdateNotAvailable();
     void updateFailed(int consecutiveFailureCount, QString lastReason);
@@ -128,6 +133,7 @@ private Q_SLOTS:
     void clickTokenReceived(Update *app, const QString &clickToken);
 
 private:
+    static UpdateManager *m_instance;
     bool m_systemCheckingUpdate;
     bool m_clickCheckingUpdate;
     int m_checkingUpdates;
