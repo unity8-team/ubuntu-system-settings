cc3a11d25a059ce3c6ec810498bcafcf178b1d53
Ken VanDine <ken.vandine@canonical.com>
2015-04-29 09:24:13 -0500

    Include supported frameworks and arch when checking for updates

--- plugins/system-update/network/network.h
+++ plugins/system-update/network/network.h
@@ -51,6 +51,9 @@ public:
     void checkForNewVersions(QHash<QString, Update*> &apps);
     void getClickToken(Update *app, const QString &url,
                        const QString &authHeader);
+    constexpr static const char* FRAMEWORKS_FOLDER {"/usr/share/click/frameworks/"};
+    constexpr static const char* FRAMEWORKS_PATTERN {"*.framework"};
+    constexpr static const int FRAMEWORKS_EXTENSION_LENGTH = 10; // strlen(".framework")
 
 Q_SIGNALS:
     void updatesFound();
@@ -68,6 +71,13 @@ private:
     QHash<QString, Update*> m_apps;
 
     QString getUrlApps();
+
+protected:
+    virtual std::vector<std::string> get_available_frameworks();
+    virtual std::string get_architecture();
+    virtual std::string architectureFromDpkg();
+    virtual std::vector<std::string> list_folder(const std::string &folder, const std::string &pattern);
+
 };
 
 }
