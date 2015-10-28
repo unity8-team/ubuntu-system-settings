e9e768746187daec5e81fc9c39ffb4e80c2553ec
Bartosz Kosiorek <gang65@poczta.onet.pl>
2015-10-28 01:44:19 +0100

    Fix autopilot test, when system-settings is installed in /usr/local/bin/system-settings

--- tests/autopilot/ubuntu_system_settings/tests/__init__.py
+++ tests/autopilot/ubuntu_system_settings/tests/__init__.py
@@ -79,7 +79,7 @@ class UbuntuSystemSettingsTestCase(
 
     """Base class for Ubuntu System Settings."""
 
-    APP_PATH = '/usr/bin/system-settings'
+    APP_FILENAME = 'system-settings'
     DESKTOP_FILE = '/usr/share/applications/ubuntu-system-settings.desktop'
 
     def setUp(self, panel=None):
@@ -107,7 +107,7 @@ class UbuntuSystemSettingsTestCase(
         :returns: A proxy object that represents the application. Introspection
         data is retrievable via this object.
         """
-        params = [self.APP_PATH]
+        params = [self.APP_FILENAME]
         if platform.model() != 'Desktop':
             params.append('--desktop_file_hint={}'.format(self.DESKTOP_FILE))
 
