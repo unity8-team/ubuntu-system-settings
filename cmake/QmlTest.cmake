9c58200070718665b6bf8500232d5d10a006375d
jonas-drange <jonas.drange@canonical.com>
2016-05-28 16:11:51 +0200

    fix try target

--- cmake/QmlTest.cmake
+++ cmake/QmlTest.cmake
@@ -60,7 +60,6 @@ macro(add_manual_qml_test SUBPATH COMPONENT_NAME)
 
     set(qmlscene_command
         env ${qmltest_ENVIRONMENT}
-            ${XVFB_CMD}
             ${qmlscene_exe} ${CMAKE_CURRENT_SOURCE_DIR}/${qmltest_FILE}.qml
             ${qmlscene_imports}
     )
