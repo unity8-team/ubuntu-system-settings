ba62ec0294097f683717007d94698af98da3d31a
jonas-drange <jonas.drange@canonical.com>
2016-07-11 14:25:01 +0200

    add manifest test

---
+++ tests/plugins/system-update/tst_click_manifest.cpp
@@ -0,0 +1,63 @@
+/*
+ * This file is part of system-settings
+ *
+ * Copyright (C) 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * This test uses a mock web server.
+ */
+
+#include "manifest_impl.h"
+
+#include <QSignalSpy>
+#include <QTest>
+#include <QUrl>
+
+#include <QDebug>
+
+using namespace UpdatePlugin;
+
+class TstClickManifest
+    : public QObject
+{
+    Q_OBJECT
+private slots:
+    void init()
+    {
+        m_instance = new Click::ManifestImpl();
+    }
+    void cleanup()
+    {
+        QSignalSpy destroyedSpy(m_instance, SIGNAL(destroyed(QObject*)));
+        m_instance->deleteLater();
+        QTRY_COMPARE(destroyedSpy.count(), 1);
+    }
+    void testRequestSucceeded()
+    {
+        QSignalSpy requestSucceededSpy(m_instance, SIGNAL(requestSucceeded(const QJsonArray&)));
+        m_instance->request();
+        QVERIFY(requestSucceededSpy.wait());
+        QList<QVariant> args = requestSucceededSpy.takeFirst();
+        QJsonArray res = args.at(0).toJsonArray();
+        QCOMPARE(res.size(), 4); // See click.result
+    }
+private:
+    Click::ManifestImpl *m_instance = nullptr;
+};
+
+QTEST_MAIN(TstClickManifest)
+#include "tst_click_manifest.moc"
+
