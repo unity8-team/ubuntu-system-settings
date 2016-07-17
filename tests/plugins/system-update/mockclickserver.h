23e2c7af9eb49e0cf1d02510dcfb7ca6f5edf491
jonas-drange <jonas.drange@canonical.com>
2016-07-18 01:34:21 +0200

    harden further

--- tests/plugins/system-update/mockclickserver.h
+++ tests/plugins/system-update/mockclickserver.h
@@ -3,6 +3,7 @@
 
 #include <QTest>
 #include <QProcess>
+#include <QDebug>
 
 class MockClickServerTestCase
 {
@@ -13,7 +14,21 @@ protected:
         params << "mockclickserver.py";
         params << args;
         m_mockclickserver.start("python3", params);
+        QVERIFY(m_mockclickserver.waitForStarted());
+
         QTRY_COMPARE(m_mockclickserver.state(), QProcess::Running);
+
+        /* QTRY_VERIFY can't be used for reading std out for some
+        reason, so we do a manual timeout on failure to read
+        “Started” from the server’s output. */
+        for (int i = 0; i < 5; i++) {
+            QString out(m_mockclickserver.readAllStandardOutput());
+            if (out.contains("Started")) {
+                return;
+            }
+            QTest::qWait(1000);
+        }
+        QFAIL("could not start mock server in time.");
     }
 
     void stopMockClickServer()
