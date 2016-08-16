63d83fb93fe3fca75e027d411de940aab46a77c5
jonas-drange <jonas.drange@canonical.com>
2016-08-17 01:50:33 +0200

    add wifi dbushelper test

--- tests/autopilot/ubuntu_system_settings/tests/test_wifi.py
+++ tests/autopilot/ubuntu_system_settings/tests/test_wifi.py
@@ -10,15 +10,11 @@ from __future__ import absolute_import
 import dbus
 
 from autopilot.matchers import Eventually
-from dbusmock.templates.networkmanager import (
-    DEVICE_IFACE, NM80211ApSecurityFlags
-)
 from testtools.matchers import Equals
 from time import sleep
 from ubuntu_system_settings.tests import (WifiBaseTestCase,
                                           WifiWithSSIDBaseTestCase)
 from ubuntu_system_settings.utils.i18n import ugettext as _
-from unittest import skip
 
 
 class WifiEnabledTestCase(WifiBaseTestCase):
@@ -29,131 +25,6 @@ class WifiEnabledTestCase(WifiBaseTestCase):
         self.assertThat(
             self.wifi_page.title,
             Equals(_('Wi-Fi')))
-
-    def test_connect_to_hidden_network(self):
-        dialog = self.wifi_page.connect_to_hidden_network(
-            'test_ap',
-            password='abcdefgh',)
-
-        # allow backend to set up listeners
-        sleep(0.3)
-
-        if dialog:
-            dialog.wait_until_destroyed()
-
-    def test_connect_to_eduroam(self):
-        self.create_access_point(
-            'eduroam', 'eduroam',
-            security=NM80211ApSecurityFlags.NM_802_11_AP_SEC_KEY_MGMT_802_1X
-        )
-
-        dialog = self.wifi_page.connect_to_hidden_network(
-            'eduroam',
-            username='student',
-            password='a',
-            security='wpa-ep',
-            auth='peap',
-            protocol='mschapv2',
-        )
-
-        # allow backend to set up listeners
-        sleep(0.3)
-
-        if dialog:
-            dialog.wait_until_destroyed()
-
-        dev = dbus.Interface(self.dbus_con.get_object(
-            'org.freedesktop.NetworkManager', self.device_path),
-            'org.freedesktop.DBus.Properties')
-
-        conn_obj = dev.Get(
-            'org.freedesktop.NetworkManager.Device', 'AvailableConnections'
-        )[0]
-        conn = dbus.Interface(self.dbus_con.get_object(
-            'org.freedesktop.NetworkManager', conn_obj),
-            'org.freedesktop.NetworkManager.Settings.Connection')
-
-        conn_settings = conn.GetSettings()
-        wconn = conn_settings['connection']
-        w802_11_sec = conn_settings['802-11-wireless-security']
-        w802_1x = conn_settings['802-1x']
-
-        self.assertEquals(wconn['type'], '802-11-wireless')
-        self.assertEquals(w802_11_sec['key-mgmt'], 'wpa-eap')
-        self.assertIn('peap', w802_1x['eap'])
-        self.assertEquals(w802_1x['identity'], 'student')
-        self.assertEquals(w802_1x['password'], 'a')
-        self.assertEquals(w802_1x['phase2-auth'], 'mschapv2')
-
-    def test_connect_to_nonexistant_hidden_network(self):
-        dialog = self.wifi_page.connect_to_hidden_network(
-            'n/a',
-            password='abcdefgh',
-        )
-
-        # allow backend to set up listeners
-        sleep(0.3)
-
-        """Mock a StateChanged signal on the Device, using a likely
-        scenario of a not found SSID:
-            newState = 120 # NM_DEVICE_STATE_FAILED
-            oldState = 0 # does not matter
-            reason = 53 # NM_DEVICE_STATE_REASON_SSID_NOT_FOUND
-
-            We manually emit this signal, because the networkmanager mock
-            currently does not support this. See [1].
-
-            [1] https://github.com/martinpitt/python-dbusmock/issues/8
-        """
-
-        self.device_mock.EmitSignal(
-            DEVICE_IFACE, 'StateChanged', 'uuu', [120, 0, 53])
-
-        self.assertThat(
-            dialog.text, Eventually(Equals(
-                _('The Wi-Fi network could not be found'))))
-
-    @skip('skipped due to %s' % (
-        'https://github.com/martinpitt/python-dbusmock/issues/7'))
-    def test_connect_to_hidden_network_using_secrets(self):
-        dialog = self.wifi_page.connect_to_hidden_network(
-            'test_ap', security='wpa', password='abcdefgh',
-        )
-
-        # allow backend to set up listeners
-        sleep(0.3)
-
-        if dialog:
-            dialog.wait_until_destroyed()
-
-    @skip('skipped due to %s' % (
-        'https://github.com/martinpitt/python-dbusmock/issues/7'))
-    def test_connect_to_hidden_network_using_incorrect_secrets(self):
-        dialog = self.wifi_page.connect_to_hidden_network(
-            'test_ap',
-            security='wpa',
-            password='abcdefgh',
-        )
-
-        # allow backend to set up listeners
-        sleep(0.3)
-
-        self.assertThat(
-            dialog.text, Eventually(Equals(
-                _('Your authentication details were incorrect'))))
-
-    @skip('networkmanager mock does not yet support deletion of cons')
-    def test_connect_to_hidden_network_then_cancel(self):
-        dialog = self.wifi_page.connect_to_hidden_network('foo',)
-
-        # allow backend to set up listeners
-        sleep(0.3)
-
-        dialog.cancel()
-
-        self.assertThat(
-            lambda: len(self.active_connection_mock.GetMethodCalls('Delete')),
-            Eventually(Equals(1)))
 
     def test_remove_previous_network(self):
         access_points = ['Series of Tubes', 'dev/null', 'Mi-Fi',
